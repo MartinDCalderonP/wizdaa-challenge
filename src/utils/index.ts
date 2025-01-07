5fdb6c7875cd5b9b6e79a73a3ce8a3c17d6df51c
Martín <martincalderon92@outlook.com>
2025-01-07 20:15:29 -0300

    refactor: simplify formattedTime function by adding addZero utility

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,10 @@
+const addZero = (num: number): string => {
+  return num.toString().padStart(2, '0')
+}
+
 export const formattedTime = (time: number): string => {
   const minutes = Math.floor(time / 60)
   const seconds = time % 60
-  return `${minutes.toString().padStart(2, '0')}:${seconds
-    .toString()
-    .padStart(2, '0')}`
+
+  return `${addZero(minutes)}:${addZero(seconds)}`
 }
